e9056fb18c6d308c4d3ffeb69c073bfa6aa1080f
sauravgiet <asopasaurav88@gmail.com>
2018-01-04 00:59:54 +0530

    added code for header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import {MdMenuModule, MdButtonModule, MdIconModule, MdCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
